296e4b945cb35a29d4d9355956260de4d2283a76
Nilamber <nilambersingh@gmail.com>
2024-07-21 22:11:40 +0530

    push addon

---
+++ coral/src/test/java/backendTeam/officestaff.java
@@ -0,0 +1,11 @@
+package backendTeam;
+
+public class officestaff {
+
+	public static void main(String[] args) {
+
+		System.out.println("employees no. total is 543");
+		System.out.println("staff added successfully");
+	}
+
+}
---
+++ coral/src/test/java/salesTeam/Marketingstaff.java
@@ -0,0 +1,11 @@
+package salesTeam;
+
+public class Marketingstaff {
+
+	public static void main(String[] args) {
+
+		System.out.println("field total no.employee=790 ");
+		System.out.println("staff added successfully");
+	}
+
+}
