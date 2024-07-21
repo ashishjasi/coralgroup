256dd587577ab49f3f4e9a36a7f2f45f48265d5d
arjune221105 <shakti221105@gmail.com>
2024-07-21 10:18:00 -0700

    add Sumsung Brand

---
+++ coral/src/test/java/J7Prime/SumsungBrand.java
@@ -0,0 +1,10 @@
+package J7Prime;
+
+public class SumsungBrand {
+	public static void main(String[] args) {
+		System.out.println("Smasung J2");
+		System.out.println("Samsung J7");
+		System.out.println("Samsung G10");
+	}
+
+}
